671bd24473d4335c6ca900d4a2a9f8c37ed36df8
Amy <amyhan00218@hotmail.com>
2024-10-08 20:16:36 -0300

    ahhhh can't get animation lol

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -2,10 +2,21 @@ extends CharacterBody2D
 
 var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 215
+@onready var animations = $AnimationsPlayer
 
-func _process(delta):
+func handleInput():
 	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction*speed
+	
+func updateAnimation():
+	var direction = "Down"
+	if velocity.x < 0: direction = "Left"
+	elif velocity.x > 0: direction = "Right"
+	elif velocity.y <0: direction = "Up"
+	
+	animations.play("walk" + direction)
 	
 func _physics_process(delta):
-	velocity = direction * speed
+	handleInput()
 	move_and_slide()
+	updateAnimation
